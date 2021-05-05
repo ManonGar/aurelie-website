ac8ef8101be7bc9f1bd12d59a95c1aed55e11c91
Manon Garnier <manon.garnier.lehy@gmail.com>
2021-05-05 16:12:10 +0200

    notebooks images ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,65 +29,65 @@ puts "#{Theme.all.length} themes created"
 #-------------------------------Notebooks----------------------------------#
 puts 'Creating Drawings'
 
-drawing1 = Drawing.create(title: "Cascade Cities", date: 2019, theme_id: theme1.id)
+cascade_cities = Drawing.create(title: "Cascade Cities", date: 2019, theme_id: theme1.id)
 notebook1 = File.open('app/assets/images/cascade_cities.jpg')
-drawing1.photo.attach(io: notebook1, filename: 'notebook.png', content_type: 'image/png')
+cascade_cities.photo.attach(io: notebook1, filename: 'notebook.png', content_type: 'image/png')
 
-drawing2 = Drawing.create(title: "Cinq sur Cinq", date: 2021, theme_id: theme1.id)
+cinq_sur_cinq = Drawing.create(title: "Cinq sur Cinq", date: 2021, theme_id: theme1.id)
 notebook2 = File.open('app/assets/images/cinq_sur_cinq.jpg')
-drawing2.photo.attach(io: notebook2, filename: 'notebook.png', content_type: 'image/png')
+cinq_sur_cinq.photo.attach(io: notebook2, filename: 'notebook.png', content_type: 'image/png')
 
-drawing3 = Drawing.create(title: "Circle of life", date: 2021, theme_id: theme1.id)
+circle_of_life = Drawing.create(title: "Circle of life", date: 2021, theme_id: theme1.id)
 notebook3 = File.open('app/assets/images/circle_of_life.jpg')
-drawing3.photo.attach(io: notebook3, filename: 'notebook.png', content_type: 'image/png')
+circle_of_life.photo.attach(io: notebook3, filename: 'notebook.png', content_type: 'image/png')
 
-drawing4 = Drawing.create(title: "Crumbling palaces", date: 2020, theme_id: theme1.id)
-notebook4 = File.open('app/assets/images/crumbling_places.jpg')
-drawing4.photo.attach(io: notebook4, filename: 'notebook.png', content_type: 'image/png')
+crumbling_palaces = Drawing.create(title: "Crumbling palaces", date: 2020, theme_id: theme1.id)
+notebook4 = File.open('app/assets/images/crumbling_palaces.jpg')
+crumbling_palaces.photo.attach(io: notebook4, filename: 'notebook.png', content_type: 'image/png')
 
-# drawing5 = Drawing.create(title: "Darling Manhattan", date: 2020, theme_id: theme1.id)
-# notebook5 = File.open('app/assets/images/darling_manhattan.jpg')
-# drawing5.photo.attach(io: notebook5, filename: 'notebook.png', content_type: 'image/png')
+le_cinquieme_element = Drawing.create(title: "Le cinquième élément", date: 2021, theme_id: theme1.id)
+notebook5 = File.open('app/assets/images/le_cinquieme_element.jpg')
+le_cinquieme_element.photo.attach(io: notebook5, filename: 'notebook.png', content_type: 'image/png')
 
-drawing6 = Drawing.create(title: "Le cinquième élément", date: 2021, theme_id: theme1.id)
-notebook6 = File.open('app/assets/images/le_cinquieme_element.jpg')
-drawing6.photo.attach(io: notebook6, filename: 'notebook.png', content_type: 'image/png')
+le_vieil_amour = Drawing.create(title: "Le vieil amour", date: 2016, theme_id: theme1.id)
+notebook6 = File.open('app/assets/images/le_vieil_amour.jpg')
+le_vieil_amour.photo.attach(io: notebook6, filename: 'notebook.png', content_type: 'image/png')
 
-drawing7 = Drawing.create(title: "Le vieil amour", date: 2016, theme_id: theme1.id)
-notebook7 = File.open('app/assets/images/le_vieil_amour.jpg')
-drawing7.photo.attach(io: notebook7, filename: 'notebook.png', content_type: 'image/png')
+maps_rivers = Drawing.create(title: "Maps & Rivers", date: 2020, theme_id: theme1.id)
+notebook7 = File.open('app/assets/images/maps_&_rivers.jpg')
+maps_rivers.photo.attach(io: notebook7, filename: 'notebook.png', content_type: 'image/png')
 
-drawing8 = Drawing.create(title: "Maps & Rivers", date: 2020, theme_id: theme1.id)
-notebook8 = File.open('app/assets/images/maps_&_rivers.jpg')
-drawing8.photo.attach(io: notebook8, filename: 'notebook.png', content_type: 'image/png')
+medusa_perseus = Drawing.create(title: "Medusa vs. Perseus", date: 2021, theme_id: theme1.id)
+notebook8 = File.open('app/assets/images/medusa_vs_perseus.jpg')
+medusa_perseus.photo.attach(io: notebook8, filename: 'notebook.png', content_type: 'image/png')
 
-drawing9 = Drawing.create(title: "Medusa vs. Perseus", date: 2021, theme_id: theme1.id)
-notebook9 = File.open('app/assets/images/medusa_vs_perseus.jpg')
-drawing9.photo.attach(io: notebook9, filename: 'notebook.png', content_type: 'image/png')
+metropolis_blueprint = Drawing.create(title: "Metropolis blueprint", date: 2020, theme_id: theme1.id)
+notebook9 = File.open('app/assets/images/metropolis_blueprint.jpg')
+metropolis_blueprint.photo.attach(io: notebook9, filename: 'notebook.png', content_type: 'image/png')
 
-drawing10 = Drawing.create(title: "Metropolis blueprint", date: 2020, theme_id: theme1.id)
-notebook10 = File.open('app/assets/images/metropolis_blueprint.jpg')
-drawing10.photo.attach(io: notebook10, filename: 'notebook.png', content_type: 'image/png')
+montagne_de_confinements = Drawing.create(title: "Montagne de confinements", date: 2020, theme_id: theme1.id)
+notebook10 = File.open('app/assets/images/montagne_de_confinements.jpg')
+montagne_de_confinements.photo.attach(io: notebook10, filename: 'notebook.png', content_type: 'image/png')
 
-# drawing11 = Drawing.create(title: "Montagne de confinements", date: 2020, theme_id: theme1.id)
-# notebook11 = File.open('app/assets/images/montagne_de_confinements.jpg')
-# drawing11.photo.attach(io: notebook11, filename: 'notebook.png', content_type: 'image/png')
+vegetabilis = Drawing.create(title: "Vegetabilis", date: 2019, theme_id: theme1.id)
+notebook11 = File.open('app/assets/images/vegetabilis.jpg')
+vegetabilis.photo.attach(io: notebook11, filename: 'notebook.png', content_type: 'image/png')
 
-drawing12 = Drawing.create(title: "Vegetabilis", date: 2019, theme_id: theme1.id)
-notebook12 = File.open('app/assets/images/vegetabilis.jpg')
-drawing12.photo.attach(io: notebook12, filename: 'notebook.png', content_type: 'image/png')
+the_other_places = Drawing.create(title: "The other places", date: 2021, theme_id: theme1.id)
+notebook12 = File.open('app/assets/images/the_other_places.jpg')
+the_other_places.photo.attach(io: notebook12, filename: 'notebook.png', content_type: 'image/png')
 
-drawing13 = Drawing.create(title: "The other places", date: 2021, theme_id: theme1.id)
-notebook13 = File.open('app/assets/images/the_other_places.jpg')
-drawing13.photo.attach(io: notebook13, filename: 'notebook.png', content_type: 'image/png')
+les_cartes_perdues = Drawing.create(title: "Les cartes perdues", date: 2020, theme_id: theme1.id)
+notebook13 = File.open('app/assets/images/les_cartes_perdues.jpg')
+les_cartes_perdues.photo.attach(io: notebook13, filename: 'notebook.png', content_type: 'image/png')
 
-# drawing14 = Drawing.create(title: "Les cartes perdues", date: 2020, theme_id: theme1.id)
-# notebook14 = File.open('app/assets/images/les_cartes_perdues.jpg')
-# drawing14.photo.attach(io: notebook14, filename: 'notebook.png', content_type: 'image/png')
+overgrown_warships = Drawing.create(title: "Overgrown warships", date: 2019, theme_id: theme1.id)
+notebook14 = File.open('app/assets/images/overgrown_warships.jpg')
+overgrown_warships.photo.attach(io: notebook14, filename: 'notebook.png', content_type: 'image/png')
 
-# drawing15 = Drawing.create(title: "Overgrown warships", date: 2019, theme_id: theme1.id)
-# notebook15 = File.open('app/assets/images/overgrown_warships.jpg')
-# drawing15.photo.attach(io: notebook15, filename: 'notebook.png', content_type: 'image/png')
+les_profondeurs = Drawing.create(title: "Les profondeurs", date: 2019, theme_id: theme1.id)
+notebook15 = File.open('app/assets/images/les_profondeurs.jpg')
+les_profondeurs.photo.attach(io: notebook15, filename: 'notebook.png', content_type: 'image/png')
 
 #-------------------illustrations---------------------------------#
 
@@ -106,10 +106,6 @@ alice.photo.attach(io: illu1, filename: 'notebook.png', content_type: 'image/png
 geometrie = Drawing.create(title: "Géométrie", date: 2016, theme_id: theme2.id)
 illu3 = File.open('app/assets/images/geometrie.jpg')
 geometrie.photo.attach(io: illu3, filename: 'notebook.png', content_type: 'image/png')
-
-cadre_perdu = Drawing.create(title: "Le cadre perdu", date: 2016, theme_id: theme2.id)
-illu4 = File.open('app/assets/images/le_cadre_perdu.jpg')
-cadre_perdu.photo.attach(io: illu4, filename: 'notebook.png', content_type: 'image/png')
 
 enfer = Drawing.create(title: "L'Enfer", date: 2016, theme_id: theme2.id)
 illu5 = File.open('app/assets/images/enfer.jpg')
